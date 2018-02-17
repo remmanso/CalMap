39c69a6b0a6b18623cef52865f939337b29216f8
Grimwood <remmanso@gmail.com>
2018-02-17 16:18:12 +0100

    First Push

---
+++ CalMapManager/CalMapManager/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CalMapManager.Models;
+
+namespace CalMapManager.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
